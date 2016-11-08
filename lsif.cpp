4473127f197a51d3f1b77d8d5f6e70d88578f416
Andrew Nelless <andrew@symless.com>
2016-11-08 11:51:48 +0000

    Comment the network interface list loop

--- lsif.cpp
+++ lsif.cpp
@@ -48,33 +48,40 @@ get_all_netdevices (std::size_t const num_tries = 100) {
     auto tries_left = num_tries;
 
     do {
+        /* Limit the number of attempts we try to grab all network interfaces */
         if (!tries_left--) {
             throw std::runtime_error (
                 "Failed to get list of network interfaces");
         }
 
+        /* Count the number of interfaces on the machine (null buffer) */
         std::memset (&conf, 0, sizeof (conf));
         if (::ioctl (fd, SIOCGIFCONF, &conf)) {
             throw_errno ();
         }
 
+        /* Create a buffer of twice what we need (minimum of 16) */
         if_buf = boost::numeric_cast<std::size_t> (conf.ifc_len);
         assert ((if_buf % sizeof (struct ifreq)) == 0);
         if_buf = 2 * std::max (size_t (8), if_buf / sizeof (struct ifreq));
         interfaces.resize (if_buf);
 
+        /* Get the list of interfaces in to the new buffer */
         conf.ifc_len = boost::numeric_cast<int> (byte_size_of (interfaces));
         conf.ifc_req = interfaces.data ();
         if (::ioctl (fd, SIOCGIFCONF, &conf)) {
             throw_errno ();
         }
 
+        /* Count the number of interfaces returned  */
         if_ret = boost::numeric_cast<std::size_t> (conf.ifc_len);
         assert ((if_ret % sizeof (struct ifreq)) == 0);
         if_ret /= sizeof (struct ifreq);
-        // std::cout << if_ret << " / " << if_buf << std::endl;
+
+        /* Loop until we get a buffer that wasn't filled */
     } while (if_ret == if_buf);
 
+    /* Resize the buffer to the number actually grabbed */
     interfaces.resize (if_ret);
 
     for (auto& ifc : interfaces) {
