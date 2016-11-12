bd3e16832f16bcb9ee2399eaa8b51e081411fe06
Andrew Nelless <andrew@symless.com>
2016-11-12 15:28:58 +0000

    Minor fixes in interface filtering

--- multicast.cpp
+++ multicast.cpp
@@ -1,27 +1,27 @@
-#include <multicast.hpp>
-#include <lsif.hpp>
 #include <iostream>
+#include <lsif.hpp>
+#include <multicast.hpp>
 
 std::vector<MulticastInterface>
-get_all_multicast_interfaces() {
+get_all_multicast_interfaces () {
     std::vector<MulticastInterface> ret;
-    auto netdevices = get_all_netdevices();
+    auto netdevices = get_all_netdevices ();
 
-    auto eth_begin = netdevices.lower_bound("en");
-    auto eth_end = netdevices.upper_bound(std::string("en\xff", 3));
+    auto eth_begin = netdevices.lower_bound ("en");
+    auto eth_end   = netdevices.lower_bound (std::string ("eo", 2));
     for (auto i = eth_begin; i != eth_end; ++i) {
         MulticastInterface mifc;
-        mifc.name = i->first;
-        mifc.primary_ip = i->second.to_string();
+        mifc.name       = i->first;
+        mifc.primary_ip = i->second.to_string ();
         ret.push_back (std::move (mifc));
     }
 
-    auto wifi_begin = netdevices.lower_bound("wl");
-    auto wifi_end = netdevices.upper_bound(std::string("wl\xff", 3));
+    auto wifi_begin = netdevices.lower_bound ("wl");
+    auto wifi_end   = netdevices.lower_bound (std::string ("wm", 2));
     for (auto i = wifi_begin; i != wifi_end; ++i) {
         MulticastInterface mifc;
-        mifc.name = i->first;
-        mifc.primary_ip = i->second.to_string();
+        mifc.name       = i->first;
+        mifc.primary_ip = i->second.to_string ();
         ret.push_back (std::move (mifc));
     }
 
