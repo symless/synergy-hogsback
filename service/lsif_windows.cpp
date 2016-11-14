c6f6752b23faf0e807120cace47013d85be0db87
Andrew Nelless <andrew@symless.com>
2016-11-14 22:13:46 +0000

    Stub windows service support

---
+++ service/lsif_windows.cpp
@@ -0,0 +1,8 @@
+#include "lsif.hpp"
+#include <Iphlpapi.h>
+
+std::map<std::string, boost::asio::ip::address>
+get_all_netdevices () {
+    std::map<std::string, boost::asio::ip::address> ret;
+    return ret;
+}
