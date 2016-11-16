84ca8fabe62b6c6f46d3932899af1971c63ee5b1
Andrew Nelless <andrew@symless.com>
2016-11-16 09:53:25 +0000

    Add note about lsif

--- service/lsif.hpp
+++ service/lsif.hpp
@@ -5,6 +5,9 @@
 #include <map>
 #include <string>
 
+// TODO: return a set of network interface structs that look more like the
+// struct in multicast.hpp
+
 std::map<std::string, boost::asio::ip::address> get_all_netdevices ();
 
 #endif // LSIF_HPP
