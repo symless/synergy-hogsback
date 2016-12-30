0bb1ca46667fbe11491a593f0173f0a1817b4e60
XinyuHou <xinyu@synergy-project.org>
2016-12-30 12:22:40 +0000

    Convert tabs to spaces in ScreenListMode.h

--- gui/lib/ScreenListModel.h
+++ gui/lib/ScreenListModel.h
@@ -9,54 +9,54 @@
 
 class LIB_SPEC ScreenListModel : public QAbstractListModel
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	enum ScreenRoles {
-		kPosXRole = Qt::UserRole + 1,
-		kPosYRole,
-		kNameRole,
-		kStateImageRole
-	};
+    enum ScreenRoles {
+        kPosXRole = Qt::UserRole + 1,
+        kPosYRole,
+        kNameRole,
+        kStateImageRole
+    };
 
-	ScreenListModel();
-	virtual ~ScreenListModel();
+    ScreenListModel();
+    virtual ~ScreenListModel();
 
-	Q_PROPERTY(float scale READ scale NOTIFY scaleChanged)
+    Q_PROPERTY(float scale READ scale NOTIFY scaleChanged)
 
-	Q_INVOKABLE int screenIconWidth();
-	Q_INVOKABLE int screenIconHeight();
+    Q_INVOKABLE int screenIconWidth();
+    Q_INVOKABLE int screenIconHeight();
 
-	int rowCount(const QModelIndex& parent = QModelIndex()) const;
-	QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
-	QModelIndex getIndex(int row, int column = 0,
-			const QModelIndex& parent = QModelIndex()) const;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+    QModelIndex getIndex(int row, int column = 0,
+            const QModelIndex& parent = QModelIndex()) const;
 
-	int findScreen(QString name);
-	void addScreen(const Screen& screen);
-	void removeScreen(QString name);
-	void getScreenPos(QString name, int& newPosX,int& newPosY);
-	virtual void adjustAll(int disX, int disY);
-	int getModelIndex(int x, int y);
-	void moveModel(int index, int offsetX, int offsetY);
-	int getScreenModeSize();
-	const Screen& getScreen(int index) const;
-	void update(const QList<Screen>& screens);
+    int findScreen(QString name);
+    void addScreen(const Screen& screen);
+    void removeScreen(QString name);
+    void getScreenPos(QString name, int& newPosX,int& newPosY);
+    virtual void adjustAll(int disX, int disY);
+    int getModelIndex(int x, int y);
+    void moveModel(int index, int offsetX, int offsetY);
+    int getScreenModeSize();
+    const Screen& getScreen(int index) const;
+    void update(const QList<Screen>& screens);
     QSet<Screen> getScreenNames();
-	float scale() const;
-	void setScale(float s);
+    float scale() const;
+    void setScale(float s);
 
 signals:
-	void scaleChanged();
+    void scaleChanged();
 
 protected:
-	QHash<int, QByteArray> roleNames() const;
+    QHash<int, QByteArray> roleNames() const;
 
 private:
-	friend class ConfigMessageConvertor;
+    friend class ConfigMessageConvertor;
 
     QList<Screen> m_screens;
-	float m_scale;
+    float m_scale;
 };
 
 #endif // SERVERCONFIGURATION_H
