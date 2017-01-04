6f68225a7cf8ec051444da45e4d6044e3b1e202f
XinyuHou <xinyu@synergy-project.org>
2017-01-04 16:06:03 +0000

    Fix tabs

--- gui/main.cpp
+++ gui/main.cpp
@@ -12,24 +12,24 @@
 
 int main(int argc, char* argv[])
 {
-	QApplication app(argc, argv);
-	LogManager::instance();
+    QApplication app(argc, argv);
+    LogManager::instance();
 
-	try {
-		qmlRegisterType<Hostname>("com.synergy.gui", 1, 0, "Hostname");
-		qmlRegisterType<ScreenListModel>("com.synergy.gui", 1, 0, "ScreenListModel");
-		qmlRegisterType<ScreenManager>("com.synergy.gui", 1, 0, "ScreenManager");
-		qmlRegisterType<ProcessManager>("com.synergy.gui", 1, 0, "ProcessManager");
-		QQmlApplicationEngine engine(QUrl(QStringLiteral("qrc:/main.qml")));
+    try {
+        qmlRegisterType<Hostname>("com.synergy.gui", 1, 0, "Hostname");
+        qmlRegisterType<ScreenListModel>("com.synergy.gui", 1, 0, "ScreenListModel");
+        qmlRegisterType<ScreenManager>("com.synergy.gui", 1, 0, "ScreenManager");
+        qmlRegisterType<ProcessManager>("com.synergy.gui", 1, 0, "ProcessManager");
+        QQmlApplicationEngine engine(QUrl(QStringLiteral("qrc:/main.qml")));
 
-		QIcon icon(":res/image/synergy.ico");
-		app.setWindowIcon(icon);
+        QIcon icon(":res/image/synergy.ico");
+        app.setWindowIcon(icon);
 
-		return app.exec();
-	}
-	catch (std::runtime_error& e) {
-		LogManager::error(QString("exception catched: ")
-							.arg(e.what()));
-		return 0;
-	}
+        return app.exec();
+    }
+    catch (std::runtime_error& e) {
+        LogManager::error(QString("exception catched: ")
+                            .arg(e.what()));
+        return 0;
+    }
 }
