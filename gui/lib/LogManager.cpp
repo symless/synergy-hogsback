dd57da737c8b87193fd5dbfe8a58143a5b8baa03
XinyuHou <xinyu@synergy-project.org>
2017-01-16 18:12:59 +0000

    Always log qDebug into console

--- gui/lib/LogManager.cpp
+++ gui/lib/LogManager.cpp
@@ -13,69 +13,67 @@ QFile LogManager::s_file;
 const QString kDefaultLogFile = "synergy.log";
 
 QObject* LogManager::instance(QQmlEngine* engine,
-										QJSEngine* scriptEngine)
+                                        QJSEngine* scriptEngine)
 {
-	Q_UNUSED(engine)
-	Q_UNUSED(scriptEngine)
+    Q_UNUSED(engine)
+    Q_UNUSED(scriptEngine)
 
-	static LogManager s_instance;
-	return &s_instance;
+    static LogManager s_instance;
+    return &s_instance;
 }
 
 LogManager::LogManager()
 {
-	DirectoryManager directoryManager;
-	s_file.setFileName(directoryManager.installedDir() + kDefaultLogFile);
-	s_file.open(QIODevice::WriteOnly);
+    DirectoryManager directoryManager;
+    s_file.setFileName(directoryManager.installedDir() + kDefaultLogFile);
+    s_file.open(QIODevice::WriteOnly);
 }
 
 LogManager::~LogManager()
 {
-	if (s_file.isOpen()) {
-		s_file.close();
-	}
+    if (s_file.isOpen()) {
+        s_file.close();
+    }
 }
 
 void LogManager::row(const QString& text)
 {
-	appendRaw(text);
+    appendRaw(text);
 }
 void LogManager::error(const QString& text)
 {
-	appendRaw(timeStamp() + " ERROR: " + text);
+    appendRaw(timeStamp() + " ERROR: " + text);
 }
 
 void LogManager::warning(const QString& text)
 {
-	appendRaw(timeStamp() + " WARNNIG: " + text);
+    appendRaw(timeStamp() + " WARNNIG: " + text);
 }
 
 void LogManager::info(const QString& text)
 {
-	appendRaw(timeStamp() + " INFO: " + text);
+    appendRaw(timeStamp() + " INFO: " + text);
 }
 
 void LogManager::debug(const QString& text)
 {
 
-	appendRaw(timeStamp() + " DEBUG: " + text);
+    appendRaw(timeStamp() + " DEBUG: " + text);
 }
 
 QString LogManager::timeStamp()
 {
-	QDateTime current = QDateTime::currentDateTime();
-	return '[' + current.toString(Qt::ISODate) + ']';
+    QDateTime current = QDateTime::currentDateTime();
+    return '[' + current.toString(Qt::ISODate) + ']';
 }
 
 void LogManager::appendRaw(const QString& text)
 {
-	foreach(QString line, text.split(QRegExp("\r|\n|\r\n"))) {
-		if (!line.isEmpty()) {
-			// QTextStream stream(&s_file);
-			// stream << line << endl;
-#ifdef QT_DEBUG
-			qDebug() << line << endl;
-#endif
-		}
-	}
+    foreach(QString line, text.split(QRegExp("\r|\n|\r\n"))) {
+        if (!line.isEmpty()) {
+            // QTextStream stream(&s_file);
+            // stream << line << endl;
+            qDebug() << line << endl;
+        }
+    }
 }
