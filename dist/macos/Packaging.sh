f9c997e474ca46cdb5fca7229c09c9a1595fb2c3
Jerry (Xinyu Hou) <jerry@symless.com>
2017-03-22 12:55:28 +0000

    Change Mac dmg background and icon positions

--- dist/macos/Packaging.sh
+++ dist/macos/Packaging.sh
@@ -103,13 +103,13 @@ echo '
            set current view of container window to icon view
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {400, 100, 920, 529}
+           set the bounds of container window to {400, 100, 900, 500}
            set viewOptions to the icon view options of container window
            set arrangement of viewOptions to not arranged
            set icon size of viewOptions to 72
            set background picture of viewOptions to file ".background:'${DMG_BACKGROUND_IMG}'"
-           set position of item "'${APP_NAME}'.app" of container window to {165, 230}
-           set position of item "Applications" of container window to {360, 225}
+           set position of item "'${APP_NAME}'.app" of container window to {155, 233}
+           set position of item "Applications" of container window to {347, 232}
            close
            open
            update without registering applications
