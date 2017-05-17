159252b885e1b59d939db80a77c40e2c7c66e88c
Andrew Nelless <andrew@symless.com>
2017-05-17 01:50:29 +0100

    CMake cleanups

--- cmake/Modules/FindSodium.cmake
+++ cmake/Modules/FindSodium.cmake
@@ -1,15 +1,10 @@
 
-message (STATUS "ENV{LIBSODIUM_INCLUDE_DIR} = $ENV{LIBSODIUM_INCLUDE_DIR}")
-message (STATUS "ENV{LIBSODIUM_LIBRARY_DIR} = $ENV{LIBSODIUM_LIBRARY_DIR}")
-
 if (MSVC)
     if ((NOT DEFINED SODIUM_INCLUDE_HINTS) AND (DEFINED ENV{LIBSODIUM_INCLUDE_DIR}))
-        message ("Setting libsodium include path from environment.")
         set (SODIUM_INCLUDE_HINTS $ENV{LIBSODIUM_INCLUDE_DIR})
     endif()
 
     if ((NOT DEFINED SODIUM_LIBRARY_HINTS) AND (DEFINED ENV{LIBSODIUM_LIBRARY_DIR}))
-        message ("Setting libsodium library path from environment.")
         set (SODIUM_LIBRARY_HINTS $ENV{LIBSODIUM_LIBRARY_DIR})
     endif()
 else()
@@ -29,23 +24,17 @@ else()
     endif()
 endif()
 
-message (STATUS "SODIUM_INCLUDE_HINTS = ${SODIUM_INCLUDE_HINTS}")
-message (STATUS "SODIUM_LIBRARY_HINTS = ${SODIUM_LIBRARY_HINTS}")
-
-find_path(
+find_path (
     SODIUM_INCLUDE_DIRS
     NAMES sodium.h
     HINTS ${SODIUM_INCLUDE_HINTS}
 )
 
-find_library(
+find_library (
     SODIUM_LIBRARIES
     NAMES libsodium sodium
     HINTS ${SODIUM_LIBRARY_HINTS}
 )
-
-message (STATUS "SODIUM_INCLUDE_DIRS = ${SODIUM_INCLUDE_DIRS}")
-message (STATUS "SODIUM_LIBRARIES = ${SODIUM_LIBRARIES}")
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (SODIUM DEFAULT_MSG SODIUM_LIBRARIES SODIUM_INCLUDE_DIRS)
