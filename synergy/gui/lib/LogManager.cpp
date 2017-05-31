02d5a820ffa1c4b35abc9acdac4b6b7458673eb1
Jerry (Xinyu Hou) <jerry@symless.com>
2017-05-31 18:04:31 +0100

    #79 Generate a log file for uploading

--- synergy/gui/lib/LogManager.cpp
+++ synergy/gui/lib/LogManager.cpp
@@ -1,6 +1,8 @@
 #include "LogManager.h"
 
+#include "CloudClient.h"
 #include "DirectoryManager.h"
+#include "AppConfig.h"
 #include "Common.h"
 
 #include <QDateTime>
@@ -10,9 +12,12 @@
 
 QObject* LogManager::s_instance = NULL;
 QQmlContext* LogManager::s_qmlContext = NULL;
+CloudClient* LogManager::s_cloudClient = NULL;
 QFile LogManager::s_file;
 QStringList LogManager::s_logLines;
-int LogManager::s_maximumLogLines = 100;
+const int LogManager::s_maximumLogLines = 100;
+bool LogManager::s_uploading = false;
+const int LogManager::s_maximumUploadLogLines = 1000;
 
 const QString kDefaultLogFile = "synergy.log";
 const QString kGUILogPrefix = "[ UI ] ";
@@ -45,7 +50,18 @@ LogManager::~LogManager()
 
 void LogManager::uploadLogFile()
 {
-    LogManager::info("Sending log file...");
+    if (!s_uploading) {
+        info("Sending log file...");
+
+        s_uploading = true;
+
+        QString filename = prepareUploadFile();
+        if (!filename.isEmpty()) {
+            upload(filename);
+        }
+
+        s_uploading = false;
+    }
 }
 
 void LogManager::raw(const QString& text)
@@ -109,6 +125,93 @@ void LogManager::updateLogLineModel()
     }
     QString text = s_logLines.join('\n');
     s_qmlContext->setContextProperty("LoggingModel", QVariant::fromValue(text));
+}
+
+bool LogManager::getLastLines(int lines, QString& content)
+{
+    if (!s_file.exists()) {
+        content = "Log file doesn't exist";
+        return false;
+    }
+
+    QFile file(s_file.fileName());
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        file.seek(file.size() - 1);
+        int count = 0;
+        while (file.pos() > 0) {
+            QString ch = file.read(1);
+            if (ch == "\n") {
+                count++;
+            }
+
+            if (count <= lines) {
+                file.seek(file.pos() - 2);
+            }
+            else {
+                break;
+            }
+        }
+        content = file.readAll();
+        file.close();
+
+        return true;
+    }
+
+    return false;
+}
+
+QString LogManager::prepareUploadFile()
+{
+    QString context;
+    if (!getLastLines(s_maximumUploadLogLines, context)) {
+        return false;
+    }
+
+    QString filename = generateLogFilename();
+    QFile file(filename);
+    if (file.open(QIODevice::WriteOnly)) {
+        QTextStream stream(&file);
+        stream << context;
+        file.close();
+    }
+    else {
+        filename.clear();
+    }
+
+    return filename;
+}
+
+QString LogManager::generateLogFilename()
+{
+    // format: userID-timestamp.log
+    // example: 1-2017-05-31T16-07-40.log
+    DirectoryManager directoryManager;
+    QString filePath = directoryManager.profileDir();
+
+    AppConfig* appConfig =
+            qobject_cast<AppConfig*>(AppConfig::instance());
+    QString filename = QString::number(appConfig->userId());
+    filename += '-';
+
+    QDateTime current = QDateTime::currentDateTime();
+    filename += current.toString(Qt::ISODate);
+    filename += ".log";
+    filename.replace(':', '-');
+    filename = filePath + '/' + filename;
+
+    return filename;
+}
+
+void LogManager::upload(QString filename)
+{
+    if (s_cloudClient) {
+        s_cloudClient->uploadLogFile(filename);
+    }
+}
+
+void LogManager::setCloudClient(CloudClient* value)
+{
+    s_cloudClient = value;
 }
 
 void LogManager::setQmlContext(QQmlContext* value)
--- synergy/gui/lib/LogManager.h
+++ synergy/gui/lib/LogManager.h
@@ -9,6 +9,7 @@
 #include <QStringList>
 
 class QQmlContext;
+class CloudClient;
 
 class LIB_SPEC LogManager : public QObject
 {
@@ -22,12 +23,13 @@ public:
     Q_INVOKABLE void uploadLogFile();
 
     static void raw(const QString& text);
-	static void error(const QString& text);
-	static void warning(const QString& text);
-	static void info(const QString& text);
-	static void debug(const QString& text);
+    static void error(const QString& text);
+    static void warning(const QString& text);
+    static void info(const QString& text);
+    static void debug(const QString& text);
     static QString logFilename();
     static void setQmlContext(QQmlContext* value);
+    static void setCloudClient(CloudClient* value);
 
 private:
     LogManager();
@@ -36,12 +38,20 @@ private:
 	static void appendRaw(const QString& text);
     static void updateLogLineModel();
 
+    bool getLastLines(int lines, QString& content);
+    QString prepareUploadFile();
+    QString generateLogFilename();
+    void upload(QString filename);
+
 private:
 	static QFile s_file;
     static QStringList s_logLines;
 	static QObject* s_instance;
     static QQmlContext* s_qmlContext;
-    static int s_maximumLogLines;
+    static CloudClient* s_cloudClient;
+    static const int s_maximumLogLines;
+    static bool s_uploading;
+    static const int s_maximumUploadLogLines;
 };
 
 #endif // LOGMANAGER_H
