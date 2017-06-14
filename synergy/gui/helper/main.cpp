724bbcee59a660be17e4f07db6c8db569ff295bf
Andrew Nelless <nlyan@users.noreply.github.com>
2017-06-14 20:56:06 +0100

    #87 Fix shitty use of time()

--- synergy/gui/helper/main.cpp
+++ synergy/gui/helper/main.cpp
@@ -9,7 +9,8 @@ int
 main (int, const char*[])
 {
     std::ofstream ofs ("/Users/Andrew/test.txt");
-    ofs << "test " << ctime(time(NULL)) << "\n";
+    auto t = time(NULL);
+    ofs << "test " << ctime(&t) << "\n";
     ofs.close();
     syslog (LOG_NOTICE, "Hello world! uid = %d, euid = %d, pid = %d\n", (int) getuid(), (int) geteuid(), (int) getpid());
     sleep (10);
