2f5d8a3b598fd2eb59609874f736a75b9b00f734
Andrew Nelless <nlyan@users.noreply.github.com>
2017-06-14 20:47:47 +0100

    #87 Write to a file because the syslog apparently doesnt work

--- synergy/gui/helper/main.cpp
+++ synergy/gui/helper/main.cpp
@@ -2,10 +2,15 @@
 #include <unistd.h>
 #include <cstdio>
 #include <cstdlib>
+#include <fstream>
+#include <time.h>
 
 int
 main (int, const char*[])
 {
+    std::ofstream ofs ("/Users/Andrew/test.txt");
+    ofs << "test " << ctime(time(NULL)) << "\n";
+    ofs.close();
     syslog (LOG_NOTICE, "Hello world! uid = %d, euid = %d, pid = %d\n", (int) getuid(), (int) geteuid(), (int) getpid());
     sleep (10);
     return EXIT_SUCCESS;
