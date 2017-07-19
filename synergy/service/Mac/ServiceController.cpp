dca9fc1b089bdeb75a44db7c37916e277636bd6a
Jerry (Xinyu Hou) <jerry@symless.com>
2017-07-19 15:01:04 +0100

    #139 Add placeholder files for Mac and Linux

---
+++ synergy/service/Mac/ServiceController.cpp
@@ -0,0 +1,32 @@
+#include "../ServiceController.h"
+
+//
+//  ServiceController for Mac
+//
+
+ServiceController::ServiceController() :
+    m_install(false),
+    m_uninstall(false)
+{
+    
+}
+
+ServiceController::~ServiceController()
+{
+
+}
+
+void ServiceController::doRun()
+{
+
+}
+
+void ServiceController::install()
+{
+
+}
+
+void ServiceController::uninstall()
+{
+
+}
