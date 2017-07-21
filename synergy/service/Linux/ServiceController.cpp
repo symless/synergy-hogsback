dabed0e37fe79ced49fac3d87227c87028cdcb43
Jerry (Xinyu Hou) <jerry@symless.com>
2017-07-21 16:04:17 +0100

    #140 Initialize signal set for srevice controller on Linux

--- synergy/service/Linux/ServiceController.cpp
+++ synergy/service/Linux/ServiceController.cpp
@@ -10,11 +10,16 @@ class ServiceControllerImp {
 
 ServiceController::ServiceController() :
     m_install(false),
-    m_uninstall(false)
+    m_uninstall(false),
+    m_foreground(false),
+#if defined(SIGQUIT)
+    m_terminationSignals(m_threadIoService, SIGTERM, SIGINT, SIGQUIT)
+#else
+    m_terminationSignals(m_threadIoService, SIGTERM, SIGINT)
+#endif
 {
-    
+    m_worker = std::make_shared<ServiceWorker>(m_threadIoService);
 }
-
 ServiceController::~ServiceController()
 {
 
