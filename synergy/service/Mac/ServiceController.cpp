fec9b12cbe4ae3ac8ed862d3413485cacb09a477
Jerry (Xinyu Hou) <jerry@symless.com>
2017-07-24 13:35:34 +0100

    #140 Flesh out Mac service controller implementation

--- synergy/service/Mac/ServiceController.cpp
+++ synergy/service/Mac/ServiceController.cpp
@@ -14,7 +14,13 @@ class ServiceControllerImp {
 
 ServiceController::ServiceController() :
     m_install(false),
-    m_uninstall(false)
+    m_uninstall(false),
+    m_foreground(false),
+#if defined(SIGQUIT)
+    m_terminationSignals(m_threadIoService, SIGTERM, SIGINT, SIGQUIT)
+#else
+    m_terminationSignals(m_threadIoService, SIGTERM, SIGINT)
+#endif
 {
     
 }
@@ -26,15 +32,21 @@ ServiceController::~ServiceController()
 
 void ServiceController::doRun()
 {
-
+    setupTerminationSignals();
+    m_worker->start();
 }
 
 void ServiceController::install()
 {
-
+    // there is no implementatioon for manual installing on Mac
 }
 
 void ServiceController::uninstall()
+{
+    // there is no implementatioon for manual uninstalling on Mac
+}
+
+void ServiceController::shutdown()
 {
 
 }
