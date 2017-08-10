c9d46472a47e8d31e9a2810e8a38de0291a1a4b5
Andrew Nelless <andrew@symless.com>
2017-08-10 12:11:04 +0100

    #89 Tidy up service main()

--- synergy/service/main.cpp
+++ synergy/service/main.cpp
@@ -1,26 +1,38 @@
 #include <synergy/common/CrashHandler.h>
-#include "ServiceWorker.h"
-#include "TerminationSignalListener.h"
-
+#include <synergy/service/ServiceWorker.h>
+#include <synergy/service/TerminationSignalListener.h>
 #include <boost/asio/io_service.hpp>
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+
+static bool crashHandlerIsInstalled = false;
 
 int
-main (int argc, char* argv[]) {
-    startCrashHandler();
+main (int argc, char* argv[]) try {
+    crashHandlerIsInstalled = startCrashHandler();
 
     boost::asio::io_service ioService;
     ServiceWorker serviceWorker(ioService);
     TerminationSignalListener signalListener(ioService);
+
     signalListener.setHandler([&serviceWorker] () {
         serviceWorker.shutdown();
     });
 
-    try {
-        serviceWorker.start();
-    }
-    catch (...) {
-        return 1;
-    }
+    serviceWorker.start();
+    return EXIT_SUCCESS;
 
-    return 0;
-};
+} catch (std::exception& e) {
+    if (crashHandlerIsInstalled) {
+        throw;
+    } else {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    return EXIT_FAILURE;
+} catch (...) {
+    if (crashHandlerIsInstalled) {
+        throw;
+    }
+    return EXIT_FAILURE;
+}
