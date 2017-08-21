54f2591fa7362828bfa6d268e9547aae5280dc4e
Jerry (Xinyu Hou) <jerry@symless.com>
2017-08-21 18:25:54 +0100

    #188 Simple HTTP get session

--- synergy/service/HttpSession.cpp
+++ synergy/service/HttpSession.cpp
@@ -1,6 +1,52 @@
 #include "HttpSession.h"
 
-HttpSession::HttpSession(boost::asio::io_service& ioService)
-{
+#include <boost/asio/connect.hpp>
 
+static const int kHttpVersion = 11;
+
+HttpSession::HttpSession(boost::asio::io_service& ioService, std::string hostname, std::string port) :
+    m_session(ioService),
+    m_stream(m_session.stream())
+{
+    m_session.setHostname(hostname);
+    m_session.setPort(port);
+}
+
+void HttpSession::get(const std::string &target)
+{
+    setupRequest(http::verb::get, target);
+
+    m_session.connected.connect(
+        [this]() {
+            onSessionConnected();
+        },
+        boost::signals2::at_front
+    );
+
+    m_session.connect();
+}
+
+void HttpSession::onSessionConnected()
+{
+    http::async_write(m_stream, m_request,
+        std::bind(
+            &HttpSession::onWriteFinished,
+            this,
+            std::placeholders::_1));
+}
+
+void HttpSession::setupRequest(http::verb method, const std::string &target)
+{
+    m_request.version = kHttpVersion;
+    m_request.method(method);
+    m_request.target(target);
+    m_request.set(http::field::host, m_session.hostname().c_str());
+    // TODO: add user token
+}
+
+void HttpSession::onWriteFinished(errorCode ec)
+{
+    m_session.checkError(ec);
+
+    // TODO: do we care if the request has been received?
 }
--- synergy/service/HttpSession.h
+++ synergy/service/HttpSession.h
@@ -1,12 +1,30 @@
 #ifndef HTTPSESSION_H
 #define HTTPSESSION_H
 
+#include "synergy/service/SecuredTcpSession.h"
+
+#include <boost/beast/http.hpp>
 #include <boost/asio/io_service.hpp>
 
-class HttpSession
+namespace http = boost::beast::http;
+
+class HttpSession final
 {
 public:
-    HttpSession(boost::asio::io_service& ioService);
+    HttpSession(boost::asio::io_service& ioService, std::string hostname, std::string port);
+
+    void get(const std::string& target);
+
+private:
+    void onSessionConnected();
+    void setupRequest(http::verb method, const std::string &target);
+    void onWriteFinished(errorCode ec);
+
+private:
+    SecuredTcpSession m_session;
+    ssl::stream<tcp::socket>& m_stream;
+    http::request<http::empty_body> m_request;
+    http::response<http::string_body> m_response;
 };
 
 #endif // HTTPSESSION_H
