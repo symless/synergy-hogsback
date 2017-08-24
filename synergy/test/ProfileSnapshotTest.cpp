6f4b02a51ee2c4cb2fc3cd742fec5bea4d1a809c
Dan Sorahan <dan@symless.com>
2017-08-24 19:51:24 +0100

    #189 Unit test for profile snapshots

---
+++ synergy/test/ProfileSnapshotTest.cpp
@@ -0,0 +1,10 @@
+#include "catch.hpp"
+#include "synergy/service/ProfileSnapshot.h"
+
+TEST_CASE("Deserialize profile from JSON", "[snapshot]")
+{
+    std::string jsonMock = R"JSON({"profile":{"configVersion":0,"id":1,"name":"default","server":1},"screens":[]})JSON";
+
+    ProfileSnapshot profileSnapshot;
+    profileSnapshot.parseJsonSnapshot(jsonMock);
+}
