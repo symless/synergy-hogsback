c5e0a6481b2d634fdb9c485fb82890670d09b946
Andrew Nelless <nlyan@users.noreply.github.com>
2017-08-29 15:21:29 +0100

    #178 Remove stray try

--- synergy/service/main.cpp
+++ synergy/service/main.cpp
@@ -10,7 +10,7 @@
 static bool crashHandlerIsInstalled = false;
 
 int
-main (int argc, char* argv[]) try {
+main (int argc, char* argv[]) {
     crashHandlerIsInstalled = startCrashHandler();
 
     boost::asio::io_service ioService;
