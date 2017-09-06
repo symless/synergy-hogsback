5ddfdba851325e5de987f40adc26196baa62c843
Jerry (Xinyu Hou) <jerry@symless.com>
2017-09-06 18:08:58 +0100

    #122 Add a unit test for testing secured tcp connection

---
+++ synergy/test/SecuredTcpConnectionTest.cpp
@@ -0,0 +1,60 @@
+#include "catch.hpp"
+
+#include "synergy/service/ConnectivityTester.h"
+#include "synergy/service/SecuredTcpServer.h"
+#include "synergy/service/SecuredTcpClient.h"
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/signals2.hpp>
+
+static boost::signals2::signal<void()> testFinished;
+
+TEST_CASE("Secured TCP Connection test", "[tcp]")
+{
+    const std::string kLocalIp = "127.0.0.1";
+    const std::string kTestPort = "24811";
+    bool connected = false;
+
+    boost::asio::io_service ioService;
+
+    SecuredTcpServer server(ioService);
+    server.setAddress(kLocalIp);
+    server.setPort(kTestPort);
+    server.loadRawCetificate(
+                ConnectivityTester::testServerCertificate(),
+                ConnectivityTester::testServerKey(),
+                ConnectivityTester::testServerDH());
+
+    SecuredTcpClient client(ioService, kLocalIp, kTestPort);
+
+    server.connectFailed.connect([](SecuredTcpServer*){
+        testFinished();
+    });
+
+    client.connected.connect([&connected](SecuredTcpClient*){
+        connected = true;
+        testFinished();
+    });
+
+    client.connectFailed.connect([](SecuredTcpClient*){
+        testFinished();
+    });
+
+    testFinished.connect([&ioService]() {
+        ioService.poll();
+        ioService.stop();
+    });
+
+    boost::asio::deadline_timer timer(ioService);
+    timer.expires_from_now(boost::posix_time::seconds(3));
+    timer.async_wait([](const boost::system::error_code&) {
+        testFinished();
+    });
+
+    server.start();
+    client.connect();
+
+    ioService.run();
+
+    REQUIRE(connected == true);
+}
