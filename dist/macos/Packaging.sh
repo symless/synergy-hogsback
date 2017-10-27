a79dfc516c0458fe63d275ffa1024798712bdce1
Andrew Nelless <andrew@symless.com>
2017-10-27 14:03:35 +0100

    #267 Change macOS installer background

--- dist/macos/Packaging.sh
+++ dist/macos/Packaging.sh
@@ -102,13 +102,12 @@ echo '
            set current view of container window to icon view
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {400, 100, 900, 500}
+           set the bounds of container window to {400, 100, 796, 524}
            set viewOptions to the icon view options of container window
            set arrangement of viewOptions to not arranged
            set icon size of viewOptions to 72
            set background picture of viewOptions to file ".background:'${DMG_BACKGROUND_IMG}'"
-           set position of item "'${APP_NAME}'.app" of container window to {145, 270}
-           set position of item "Applications" of container window to {355, 270}
+           set position of item "'${APP_NAME}'.app" of container window to {198, 240}
            close
            open
            update without registering applications
