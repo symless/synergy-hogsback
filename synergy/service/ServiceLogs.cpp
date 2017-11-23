d4a55490d54ecf864e9f52d5e07a603f346c1578
Nick Bolton <nick@symless.com>
2017-11-22 18:54:34 -0800

    #257 Write to combined log file from all logs

--- synergy/service/ServiceLogs.cpp
+++ synergy/service/ServiceLogs.cpp
@@ -7,6 +7,7 @@
 using namespace boost;
 
 Log g_serviceLog;
+std::shared_ptr<spdlog::sinks::rotating_file_sink_mt> s_global = nullptr;
 
 class LogSignalSink : public spdlog::sinks::sink
 {
@@ -18,10 +19,26 @@ class LogSignalSink : public spdlog::sinks::sink
     void flush() { }
 };
 
+
+static auto
+initGlobalSink()
+{
+    if (s_global != nullptr) {
+        return s_global;
+    }
+
+    auto logDir = DirectoryManager::instance()->systemLogDir();
+    auto logPath = logDir / "synergy-combined.log";
+    s_global = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+        logPath.string(), 1024 * 1024, 1);
+    return s_global;
+}
+
 static auto
 initServiceLog()
 {
     std::vector<spdlog::sink_ptr> sinks;
+    sinks.push_back(initGlobalSink());
 
 #ifdef _WIN32
     auto console = std::make_shared<spdlog::sinks::wincolor_stdout_sink_mt>();
@@ -31,27 +48,10 @@ initServiceLog()
     sinks.push_back(console);
 
     auto logDir = DirectoryManager::instance()->systemLogDir();
-    optional<filesystem::path> logPath;
-
-    /* attempt to create log dir, report error later when logging has been
-     * setup. apparently create_directory only returns false if the dir
-     * doesn't exist (otherwise it throws). bah */
-    std::string logDirExString;
-    try {
-        if (!is_directory(logDir)) {
-            if (!create_directory(logDir)) {
-                throw std::runtime_error("directory already exists: " + logDir.string());
-            }
-        }
-
-        logPath = logDir / "synergy-service.log";
-        auto rotating = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
-            logPath->string(), 1024 * 1024, 1);
-        sinks.push_back(rotating);
-    }
-    catch (const std::exception& ex) {
-        logDirExString = ex.what();
-    }
+    auto logPath = logDir / "synergy-service.log";
+    auto rotating = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+        logPath.string(), 1024 * 1024, 1);
+    sinks.push_back(rotating);
 
     auto signal = std::make_shared<LogSignalSink>();
     sinks.push_back(signal);
@@ -61,12 +61,7 @@ initServiceLog()
     logger->flush_on(spdlog::level::debug);
     logger->set_level(spdlog::level::debug);
 
-    if (logPath) {
-        logger->debug("service log path: {}", logPath->string());
-    }
-    else if (!logDirExString.empty()) {
-        logger->warn("service file logging disabled, exception: {}", logDirExString);
-    }
+    logger->debug("service log path: {}", logPath.string());
 
     return logger;
 }
@@ -75,6 +70,7 @@ static auto
 initCoreLog()
 {
     std::vector<spdlog::sink_ptr> sinks;
+    sinks.push_back(initGlobalSink());
 
 #ifdef _WIN32
     auto console = std::make_shared<spdlog::sinks::wincolor_stdout_sink_mt>();
@@ -84,7 +80,7 @@ initCoreLog()
     sinks.push_back(console);
 
     auto logger = std::make_shared<spdlog::logger>("main", begin(sinks), end(sinks));
-    logger->set_pattern("[ Core    ] [%Y-%m-%dT%T] %l: %v");
+    logger->set_pattern("[ Core    ] %v");
     logger->flush_on(spdlog::level::debug);
     logger->set_level(spdlog::level::debug);
 
@@ -95,6 +91,7 @@ static auto
 initRouterLog()
 {
     std::vector<spdlog::sink_ptr> sinks;
+    sinks.push_back(initGlobalSink());
 
 #ifdef _WIN32
     auto console = std::make_shared<spdlog::sinks::wincolor_stdout_sink_mt>();
