4beb8793300ddf85d5c608f6747da912c911e6c7
Jerry (Xinyu Hou) <jerry@symless.com>
2018-01-22 11:40:10 +0000

    #422 Add 2 core errors detection

--- synergy/service/CoreErrorMonitor.cpp
+++ synergy/service/CoreErrorMonitor.cpp
@@ -13,5 +13,25 @@ CoreErrorMonitor::CoreErrorMonitor(const std::string &localScreenName) :
 
 void CoreErrorMonitor::monitor(CoreProcess& process)
 {
+    using boost::algorithm::contains;
 
+    process.output.connect ([this](std::string const& line) {
+        if (contains (line, "Address already in use")) {
+            error(m_localScreenName, ScreenError::kCoreZombieProcess, "");
+        }
+    });
+
+    process.output.connect ([this](std::string const& line) {
+        static boost::regex const rgx ("duplicate screen name \"(.*)\"$");
+        boost::match_results<std::string::const_iterator> results;
+        if (!regex_search (line, results, rgx)) {
+            return;
+        }
+
+        auto duplicateName = results[1].str();
+        if (!duplicateName.empty()) {
+            std::string message = "Found a duplicate screen: " + duplicateName + " in configure file.";
+            error(m_localScreenName, ScreenError::kCoreDuplicateName, message);
+        }
+    });
 }
