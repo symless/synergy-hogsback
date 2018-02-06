70712aba8e946914ddde9395aa3939d43c9b9ce8
Jerry (Xinyu Hou) <jerry@symless.com>
2018-02-06 14:46:07 +0000

    #120 Stop reconnect timer on websocket shutdown

--- synergy/service/WebsocketSession.cpp
+++ synergy/service/WebsocketSession.cpp
@@ -264,6 +264,8 @@ void WebsocketSession::shutdown() noexcept
     if (m_connected) {
         serviceLog()->debug("closing existing websocket connection");
         errorCode ec;
+        m_reconnectTimer.cancel();
+        m_reconnectTimer.get_io_service().poll();
         m_websocket->close(websocket::close_code::normal, ec);
         m_connecting = false;
         m_connected = false;
