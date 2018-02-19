7e7cd5f32eb6197c6dc4dbb4d39bce60db2c3efc
Andrew Nelless <andrew@symless.com>
2018-02-19 17:10:34 +0000

    #455 Synchronize Tray UI start with RPC ready signal

--- synergy/tray/main.cpp
+++ synergy/tray/main.cpp
@@ -3,6 +3,12 @@
 #include <QMenu>
 #include <QIcon>
 #include <synergy/tray/Controls.h>
+#include <mutex>
+#include <condition_variable>
+
+static std::mutex g_controlsMutex;
+static bool g_controlsReady = false;
+static std::condition_variable g_controlsReadyCV;
 
 int
 main (int argc, char* argv[]) {
@@ -21,6 +27,22 @@ main (int argc, char* argv[]) {
     tray.setIcon(trayIcon);
     tray.setContextMenu (&trayMenu);
     tray.show();
+
+    trayControls.ready.connect ([&]() {
+        g_controlsMutex.lock();
+        g_controlsReady = true;
+        g_controlsMutex.unlock();
+        g_controlsReadyCV.notify_all();
+    });
+
+    trayControls.connect();
+
+    std::unique_lock<std::mutex> lock (g_controlsMutex);
+    if (!g_controlsReadyCV.wait_for (lock, std::chrono::seconds(5), [&]() {
+        return g_controlsReady;
+    })) {
+        return EXIT_FAILURE;
+    }
 
     app.exec();
 }
