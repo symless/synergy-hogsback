04e742179cd8695a6300dcc78338fafeaaeff16f
Andrew Nelless <andrew@symless.com>
2018-02-26 16:11:09 +0000

    #455 Shutdown websocket gracefully-ish

--- synergy/service/WebsocketSession.cpp
+++ synergy/service/WebsocketSession.cpp
@@ -265,6 +265,8 @@ void WebsocketSession::shutdown() noexcept
         serviceLog()->debug("closing existing websocket connection");
         errorCode ec;
         m_reconnectTimer.cancel();
+        m_websocket->lowest_layer().cancel();
+        m_ioService.poll();
         m_websocket->close(websocket::close_code::normal, ec);
         m_connecting = false;
         m_connected = false;
