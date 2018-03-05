fe4c4a3f741b7be59e43aff4dda68232e2ee3bca
Jerry (Xinyu Hou) <jerry@symless.com>
2018-03-05 14:39:42 +0000

    #455 Manually specify plugin directory

--- synergy/tray/main.cpp
+++ synergy/tray/main.cpp
@@ -15,6 +15,16 @@ main (int argc, char* argv[]) {
     } catch (...) {
     }
 
+#ifdef Q_OS_OSX
+    // Hack: manually specify plugin directory
+    // reason: because we move tray process into resources folder to stop
+    // OS thinking it as an instance of UI, tray can't seem to find the
+    // plugin
+    // suggestion: use a separate bundle for tray so it would have it's own
+    // plist and plugins
+    QCoreApplication::addLibraryPath("/Applications/Synergy.app/Contents/PlugIns");
+#endif
+
     QApplication app (argc, argv);
     Tray tray;
 
